921e99f62d5977ab2e12379bdbe4c4e0dfaf1344
Simangeletutubala <simangeletutubala@gmail.com>
2024-06-18 16:21:25 +0200

    Updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Creates a unique FileStorage instance for the application"""
+"""Creates a unique storage instance for the application"""
 
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from os import getenv
 
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
+
+if HBNB_TYPE_STORAGE == "db":
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
