709f284ebf24c0d95d3fbb3eef6b897c53e56aa3
Simangeletutubala <simangeletutubala@gmail.com>
2024-06-18 16:29:52 +0200

    Updates

--- models/place.py
+++ models/place.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This module creates a Place class"""
 import models
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
 from os import getenv
 import sqlalchemy
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
@@ -27,13 +27,21 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    
+    # Define the relationship with Review
     reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
-
+    
+    # Define the many-to-many relationship with Amenity
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    
     @property
     def reviews(self):
         return [review for review in self.reviews if review.place_id == self.id]
     
     @amenities.setter
     def amenities(self, obj):
-            if isinstance(obj, Amenity):
-                self.amenity_ids.append(obj.id)
+        if isinstance(obj, Amenity):
+            if obj not in self.amenities:
+                self.amenities.append(obj)
+        else:
+            return
