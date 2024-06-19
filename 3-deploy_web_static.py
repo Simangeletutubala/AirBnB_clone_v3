9f3babe7f2b4ef474474f4fb4247ce7b321cb0b5
Simangele <144337986+Simangeletutubala@users.noreply.github.com>
2024-06-19 15:44:01 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -29,37 +29,28 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
-    """
     if exists(archive_path) is False:
-        return False  # Returns False if the file at archive_path doesnt exist
-    filename = archive_path.split('/')[-1]
-    # so now filename is <web_static_2021041409349.tgz>
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    # curr = '/data/web_static/current'
-    tmp = "/tmp/" + filename
-
+        print(f"Error: Archive {archive_path} not found.")
+        return False
+    
     try:
+        filename = archive_path.split('/')[-1]
+        no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+        tmp = "/tmp/" + filename
+        
         put(archive_path, "/tmp/")
-        # ^ Upload the archive to the /tmp/ directory of the web server
         run("mkdir -p {}/".format(no_tgz))
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
         run("tar -xzf {} -C {}/".format(tmp, no_tgz))
         run("rm {}".format(tmp))
         run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
         run("rm -rf {}/web_static".format(no_tgz))
-        # ^ Delete the archive from the web server
         run("rm -rf /data/web_static/current")
-        # Delete the symbolic link /data/web_static/current from the web server
         run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
+        
         return True
-    except:
+    except Exception as e:
+        print(f"Error during deployment: {str(e)}")
         return False
-
 
 def deploy():
     """ creates and distributes an archive to your web servers
